72924373dd217cbb7ceb018227d88d5dd90c3636
Maxim Kuznetsov <kyzya96@gmail.com>
2021-07-04 18:34:21 +0300

    init commit

---
+++ tests/test_example.py
@@ -0,0 +1,25 @@
+import os
+
+SQL_FILE = 'example.sql'
+from utils import get_db_cursor, TASKS_DIR
+
+
+def test_simple_select():
+    try:
+        f = open(os.path.join(TASKS_DIR, SQL_FILE))
+    except FileNotFoundError:
+        print(f'{SQL_FILE} not found!')
+        exit(1)
+
+    query = f.readlines()
+
+    if not query:
+        print(f'No query in file {SQL_FILE}')
+        exit(1)
+
+    cur = get_db_cursor()
+    cur.execute(query)
+
+    results = [i for i in cur.fetchall()]
+
+    assert len(results) == 3
---
+++ utils.py
@@ -0,0 +1,22 @@
+import os
+
+import psycopg2
+
+POSTGRES_HOST = os.environ.get('POSTGRES_HOST')
+POSTGRES_USER = os.environ.get('POSTGRES_USER')
+POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
+POSTGRES_PORT = os.environ.get('POSTGRES_PORT')
+
+TASKS_DIR = os.path.abspath('tasks')
+
+
+def get_db_connect():
+    return psycopg2.connect(
+        host=POSTGRES_HOST,
+        database=POSTGRES_USER,
+        user=POSTGRES_PASSWORD,
+        password=POSTGRES_PORT)
+
+
+def get_db_cursor():
+    return get_db_connect().cursor()
