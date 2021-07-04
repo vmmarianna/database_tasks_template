52c415a8e0909c3a2d8149b6a99b5c90baf2434a
Maxim Kuznetsov <kyzya96@gmail.com>
2021-07-04 19:12:04 +0300

    fixing ci

--- upload_dump.py
+++ upload_dump.py
@@ -1,7 +1,8 @@
-from tests.utils import get_db_cursor
-
-curr = get_db_cursor()
+from tests.utils import get_db_connect
 
 query = ''.join(open('dump.sql').readlines())
 
+conn = get_db_connect()
+curr = conn.cursor()
 curr.execute(query)
+conn.commit()
