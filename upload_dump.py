45d8cfc95f8285c969ee0a24c11663395991ac42
Maxim Kuznetsov <kyzya96@gmail.com>
2021-07-08 19:54:51 +0300

    fixing ci

--- upload_dump.py
+++ upload_dump.py
@@ -1,6 +1,6 @@
 from tests.utils import get_db_connect
 
-query = ''.join(open('buses1.sql.sql').readlines())
+query = ''.join(open('buses1.sql').readlines())
 
 conn = get_db_connect()
 curr = conn.cursor()
